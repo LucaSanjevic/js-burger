f566278788323b636194c7271807de5cfcf37a13
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-04-19 18:38:34 +0200

    fatto

--- myScript.js
+++ myScript.js
@@ -7,10 +7,12 @@ document.getElementById("btnPrice").addEventListener("click", function() {
     var btnPomodoro = document.getElementById("Pomodoro")
     var btnLattuga = document.getElementById("Lattuga")
     var btnKetchup = document.getElementById("Ketchup")
+    var sconto = document.getElementById("coupon").value
+
 var price = 1;
 
     if(btnFormaggio.checked) {
-        price += 2
+        price += 2.50
     }
 
     if(btnUova.checked) {
@@ -18,19 +20,23 @@ var price = 1;
     }
     
      if(btnMostarda.checked) {
-        price += 2
+        price += 1
     }
     
     if(btnPomodoro.checked) {
-        price += 2
+        price += 1.50
     }
     
     if(btnLattuga.checked) {
-        price += 2
+        price += 0.50
     }
     
     if(btnKetchup.checked) {
-        price += 2
+        price += 0.10
+    }
+
+    if(sconto == "936193BSOBDW" || sconto == "739624FJCHEY") {
+    price = (price/100) * 80
     }
 
         document.getElementById("price").innerHTML = price;

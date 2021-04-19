52aaf534c27d1f38d79c3a113ecd6aeda8d15b2c
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-04-19 17:05:14 +0200

    modifiche

--- myScript.js
+++ myScript.js
@@ -1,60 +1,38 @@
-var priceFormaggio = false;
-var priceUova = false;
-var priceMostarda = false;
-var pricePomodoro = false;
-var priceLattuga = false;
-var priceKetchup = false;
-
-
-document.getElementById("btnFormaggio").addEventListener("click", function(priceFormaggio) {
-    return true
-})
-
-document.getElementById("btnUova").addEventListener("click", function(priceUova) {
-    return true
-})
-
-document.getElementById("btnMostarda").addEventListener("click", function(priceMostarda) {
-    return true
-})
-
-document.getElementById("btnPomodoro").addEventListener("click", function(pricePomodoro) {
-    return true
-})
-
-document.getElementById("btnLattuga").addEventListener("click", function(priceLattuga) {
-    return true
-})
-
-document.getElementById("btnKetchup").addEventListener("click", function(priceKetchup) {
-    return true
-})
 
 document.getElementById("btnPrice").addEventListener("click", function() {
-    var price = 0;
+    
+    var btnFormaggio = document.getElementById("Formaggio")
+    var btnUova = document.getElementById("Uova")
+    var btnMostarda = document.getElementById("Mostarda")
+    var btnPomodoro = document.getElementById("Pomodoro")
+    var btnLattuga = document.getElementById("Lattuga")
+    var btnKetchup = document.getElementById("Ketchup")
+var price = 5;
 
-    if(priceFormaggio == true) {
-        price = price + 2;
+    if(btnFormaggio.checked) {
+        price += 2
     }
 
-    if(priceUova == true) {
-        price = price + 2.50;
+    if(btnUova.checked) {
+        price += 2
+    }
+    
+     if(btnMostarda.checked) {
+        price += 2
+    }
+    
+    if(btnPomodoro.checked) {
+        price += 2
+    }
+    
+    if(btnLattuga.checked) {
+        price += 2
+    }
+    
+    if(btnKetchup.checked) {
+        price += 2
     }
 
-    if(priceMostarda == true) {
-        price = price + 0.50;
-    }
-
-    if(pricePomodoro == true) {
-        price = price + 1;
-    }
-
-    if(priceLattuga == true) {
-        price = price + 0.50;
-    }
-
-    if(priceKetchup == true) {
-        price = price + 0.10;
-    }
-    document.getElementById("price").innerHTML = price;
-})
+        document.getElementById("price").innerHTML = price;
+    })
+    
